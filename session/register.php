2b114c23f2a2f4e6f80c6aab08b42c310d33a23a
Stanislav Pekarovic <spekarovic@gmail.com>
2018-09-02 17:17:29 +0200

    Register fix

--- session/register.php
+++ session/register.php
@@ -112,7 +112,7 @@
                   if (mysqli_stmt_bind_param($stmt,"sssssssii",$username,$name,$surname,$email,$password,$date,$team,$exp,$privilege)){
                     if (mysqli_stmt_execute($stmt)){
                       // Záznam sa podarilo uložiť
-                      echo '<script>alert("Registrácia bola úspešná, môžete sa prihlásiť!");window.location.replace("../login.php");</script>';
+                      echo '<script>alert("Registrácia bola úspešná, môžete sa prihlásiť!");window.location.replace("login.php");</script>';
                       mysqli_stmt_close($stmt);
                     } else{
                       // Nepodarilo sa uložiť...
